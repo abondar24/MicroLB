412f52e0e771be458e299d9c3501dfcbae270565
Alex Bondar <abondar1992@gmail.com>
2024-08-28 02:11:02 +0200

    added test server

--- testServer/server.go
+++ testServer/server.go
@@ -1,1 +1,31 @@
-package testServer
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func handler(port string) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Server is up on port %s", port)
+
+	}
+}
+
+func main() {
+	port := flag.String("port", "", "Port to run server on")
+	flag.Parse()
+
+	if *port == "" {
+		log.Fatal("You must specify a port to run server on using -port flag")
+
+	}
+
+	http.HandleFunc("/", handler(*port))
+	err := http.ListenAndServe(":"+*port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
