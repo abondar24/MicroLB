181c77d7c577edfdc3c532c37eb48b9d1af3bf64
Alex Bondar <abondar1992@gmail.com>
2024-08-30 14:19:05 +0200

    added response to test server

--- testServer/server.go
+++ testServer/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -10,7 +11,13 @@ func handler(port string) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Server is up on port %s", port)
-
+		response := fmt.Sprintf("Server is running on port %s", port)
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte(response))
+		if err != nil {
+			log.Printf("Error writing response: %v", err)
+		}
 	}
 }
 
